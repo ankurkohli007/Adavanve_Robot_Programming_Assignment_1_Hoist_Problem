097087824ccaf9dbc29cf70e5c1c728f1b2d58cf
ankurkohli007 <ankurkohli1997007@gmail.com>
2022-08-05 02:15:41 +0530

    first commit

---
+++ help.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+more -d ProjectDescription.txt
+more -d ./$X/master/master.txt
+more -d ./$X/command/command.txt
+more -d ./$X/inspection/inspection.txt
+more -d ./$X/motor_x/motor_x.txt
+more -d ./$X/motor_z/motor_z.txt
+more -d ./$X/watchdog/watchdog.txt
